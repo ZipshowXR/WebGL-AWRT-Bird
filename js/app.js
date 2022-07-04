748ba27dfbdd86f29947d7795f0d06fb230dde40
hasel20 <zipshowxr@gmail.com>
2022-07-04 13:31:16 +0900

    a

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebGL-AWRT-Bird.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ver_Modeling_001.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
